f8daa4df3bceaa15b6d969393de36e9ba518a614
Dr. Subramanian <rsmani84@gmail.com>
2026-07-21 09:42:30 +0530

    Add visualization utility for molecule images

---
+++ Create utils/visualization.py
@@ -0,0 +1,20 @@
+from rdkit.Chem import Draw
+from rdkit import Chem
+
+import io
+
+
+def molecule_image(mol):
+
+    Chem.rdDepictor.Compute2DCoords(mol)
+
+    image = Draw.MolToImage(
+        mol,
+        size=(350,350)
+    )
+
+    buffer = io.BytesIO()
+
+    image.save(buffer, format="PNG")
+
+    return buffer.getvalue()
