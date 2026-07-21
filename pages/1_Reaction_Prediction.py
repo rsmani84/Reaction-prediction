3f59811224b2f3715a20f1a2381bd94a169f5891
Dr. Subramanian <rsmani84@gmail.com>
2026-07-21 11:20:47 +0530

    Create Organic Reaction Prediction page
    Added a Streamlit app for organic reaction prediction.

---
+++ pages/1_Reaction_Prediction.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title("🧪 Organic Reaction Prediction")
+st. write(
+    """
+    Welcome to the Organic Reaction Prediction module.
+    """
+)
+st.info("This page is under development.")
