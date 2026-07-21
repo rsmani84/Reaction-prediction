bbb20aa897d79f7238d0bc08db9e2ad709f8ea8f
Dr. Subramanian <rsmani84@gmail.com>
2026-07-21 09:53:51 +0530

    Create chemistry.py

---
+++ utils/chemistry.py
@@ -0,0 +1,21 @@
+from rdkit import Chem
+from rdkit.Chem import Descriptors
+from rdkit.Chem import rdMolDescriptors
+def load_molecule(smiles):
+   mol = Chem.MolFromSmiles(smiles)
+    if mol is None:
+        return None
+    Chem.SanitizeMol(mol)
+    return mol
+def molecular_formula(mol):
+    return rdMolDescriptors.CalcMolFormula(mol)
+def molecular_weight(mol):
+    return Descriptors.MolWt(mol)
+def logP(mol):
+    return Descriptors.MolLogP(mol)
+def tpsa(mol):
+    return Descriptors.TPSA(mol)
+def hbond_donors(mol):
+    return Descriptors.NumHDonors(mol)
+def hbond_acceptors(mol):
+   return Descriptors.NumHAcceptors(mol)
